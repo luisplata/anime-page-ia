0d4b1eb6883b28a20684d0df6cbd2ba7001ad9be
Luis Enrique Plata Osorio <www.luisplata@gmail.com>
2025-06-27 19:49:33 +0000

    updare version

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pp5fma31q9g"
+    "revision": "0.gg3ashvfoco"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
